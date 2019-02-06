5427317631386e8605e345a173dc161bc7cec474
Miklos Cari <37194836+mikloscari@users.noreply.github.com>
2019-02-06 03:04:15 -0400

    Update and rename config-iis-v1.ps1 to config-iis.ps1

--- scripts/config-iis.ps1
+++ scripts/config-iis.ps1
@@ -3,4 +3,4 @@ dism /online /enable-feature /featurename:IIS-WebServerRole
 # Set the home page.
 Set-Content ` 
   -Path "C:\\inetpub\\wwwroot\\Default.htm" ` 
-  -Value "<html><body><h2>Bienvenido a Microsoft Azure!! Mi nombre es $($env:computername).</h2></body></html>"
+  -Value "<html><body><h2>Bienvenido a Microsoft Azure! Mi nombre es $($env:computername).</h2></body></html>"
